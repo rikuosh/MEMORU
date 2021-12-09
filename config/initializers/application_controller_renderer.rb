47e16860685c01957da3d304c8d12014f58b6fe8
rikuosh <0429riku@gmail.com>
2021-12-09 12:39:15 +0000

    add post_images function

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '373a3c60be0c12e76133e09bd33bb4c80dea32ddf47e65e159bbb0740810acf5e2d891e7e1be316757499faf82abb7dd86235711494f5c027baf5158b3755d6e'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to:'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  
 end
